3454eef0c7e32b3c6e7c89de060228e441608922
Alvin <alvin.tol@hotmail.com>
2022-08-09 20:10:12 -0600

    adds utc date conversion to date route

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,14 +21,16 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 app.get("/api/:date", (req, res) => {
   const date = new Date(req.params.date);
-  const unix = Math.floor(date.getTime() / 1000);
+  const unix = Math.floor(date.getTime());
+  const utc = date.toUTCString();
   res.json({
-    unix
+    unix,
+    utc
   })
 })
 
